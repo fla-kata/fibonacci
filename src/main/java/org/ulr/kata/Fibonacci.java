5887cd50878e37a9f18bcc8c82bfec97cfd9751e
Francois Lagoueyte <francois.lagoueyte@asso-cocktail.fr>
2018-03-01 22:01:35 +0100

    ajout resultat attendu

--- src/main/java/org/ulr/kata/Fibonacci.java
+++ src/main/java/org/ulr/kata/Fibonacci.java
@@ -25,6 +25,10 @@ package org.ulr.kata;
 
 public class Fibonacci {
 
+    /**
+     * 0   1   2   3   4   5   6   7   8   9   10
+     * 0   1   1   2   3   5   8   13  21  34  55
+     */
     public int fibonacci(int n) {
         return n;
     }
