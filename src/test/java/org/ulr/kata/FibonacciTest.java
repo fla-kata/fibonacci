d6371d3e7fa6ab1cfcc46dba2481d4a870f22376
Francois Lagoueyte <francois.lagoueyte@asso-cocktail.fr>
2018-03-01 21:51:09 +0100

    init kata

---
+++ src/test/java/org/ulr/kata/FibonacciTest.java
@@ -0,0 +1,14 @@
+package org.ulr.kata;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FibonacciTest {
+
+    @Test
+    public void testFibonacci() {
+        fail("Not yet implemented");
+    }
+
+}
