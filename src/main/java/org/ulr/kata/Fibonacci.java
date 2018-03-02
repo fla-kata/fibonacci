ed3b03e1ff6db90bb0f3d4ebcecbae045c651f3e
Francois Lagoueyte <francois.lagoueyte@asso-cocktail.fr>
2018-03-02 21:14:21 +0100

    ajout exigences

--- src/main/java/org/ulr/kata/Fibonacci.java
+++ src/main/java/org/ulr/kata/Fibonacci.java
@@ -26,6 +26,7 @@ package org.ulr.kata;
 public class Fibonacci {
 
     /**
+     * Exigences :
      * 0   1   2   3   4   5   6   7   8   9   10
      * 0   1   1   2   3   5   8   13  21  34  55
      */
